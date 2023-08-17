350c14f758251c27b42ed377effd0297b5dc935f
Sushil787 <itsushil100@outlook.com>
2023-08-17 22:31:42 +0545

    project initialized:

---
+++ app/src/main/java/CSE/DeX/dexone/MainActivity.kt
@@ -0,0 +1,15 @@
+package CSE.DeX.dexone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import com.bumptech.glide.Glide
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main);
+
+    }
+}
+
